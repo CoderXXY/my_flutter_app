6ea939a455c874072f89b8c666529fec3501f437
coderXY <coderxxy@163.com>
2023-04-02 21:48:35 +0800

    【title】简易封装和添加抽屉
    【description】
    
    【author】

---
+++ lib/homeModule/HomeListView.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'HomeModel.dart';
+
+class HomeListView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // listView cell
+    Widget _listItemBuilder(BuildContext context, int idx){
+      return Container(
+        color: Colors.white,
+        margin: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Image.network(dataSource[idx].imgUrl),
+            SizedBox(height: 30),
+            Text(
+              dataSource[idx].title,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            SizedBox(width: 100)
+          ],
+        ),
+      );
+    }
+    return ListView.builder(
+      itemCount: dataSource.length,
+        itemBuilder: _listItemBuilder
+    );
+  }
+}
--- lib/homeModule/HomeModule.dart
+++ lib/homeModule/HomeModule.dart
@@ -1,30 +1,38 @@
 import 'package:flutter/material.dart';
-import 'HomeModel.dart';
+// import 'HomeModel.dart';
+import 'HomeListView.dart';
 
 class HomeModule extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    Widget _listItemBuilder(BuildContext context, int idx){
-      return Container(
-        color: Colors.white,
-        margin: EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            Image.network(dataSource[idx].imgUrl),
-            SizedBox(height: 30),
-            Text(
-              dataSource[idx].title,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            SizedBox(width: 100)
-          ],
-        ),
-      );
-    }
+    // Widget _listItemBuilder(BuildContext context, int idx){
+    //   return Container(
+    //     color: Colors.white,
+    //     margin: EdgeInsets.all(8.0),
+    //     child: Column(
+    //       children: <Widget>[
+    //         Image.network(dataSource[idx].imgUrl),
+    //         SizedBox(height: 30),
+    //         Text(
+    //           dataSource[idx].title,
+    //           style: Theme.of(context).textTheme.titleMedium,
+    //         ),
+    //         SizedBox(width: 100)
+    //       ],
+    //     ),
+    //   );
+    // }
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        leading: IconButton(
+            onPressed: ()=> debugPrint('Navigation button is pressed'),
+            icon: Icon(
+              Icons.menu,
+              color: Colors.white,
+            )
+        ),
         title: const Text(
           'Home',
           textAlign: TextAlign.center,
@@ -34,14 +42,54 @@ class HomeModule extends StatelessWidget{
             fontWeight: FontWeight.bold
           ),
         ),
+        actions: <Widget>[
+          IconButton(
+              onPressed: ()=>debugPrint('Navigation button search_icon is pressed'),
+              icon: Icon(Icons.search)
+          )
+        ],
         backgroundColor: Colors.indigo,
       ),
       body:Center(
-        child: ListView.builder(
-            itemCount: dataSource.length,
-          itemBuilder: _listItemBuilder,
-        )
+        child: HomeListView(),
+      ),
+      drawer: Container(
+        width: 180,
+        color: Colors.white,
+        padding: EdgeInsets.all(8.0),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+                child: Text(
+                    'header'.toUpperCase(),
+                  textAlign: TextAlign.center,
+                ),
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+              ),
+            ),
+            ListTile(
+              title: Text('message', textAlign: TextAlign.right,),
+              trailing: Icon(Icons.message, color: Colors.black12,),
+            ),
+            ListTile(
+              title: Text('favorite', textAlign: TextAlign.right,),
+              trailing: Icon(Icons.favorite, color: Colors.black12,),
+            ),
+            ListTile(
+              title: Text('share', textAlign: TextAlign.right,),
+              trailing: Icon(Icons.share, color: Colors.black12,),
+            ),
+          ],
+        ),
+        // Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>[
+        //     Text('this is drawer')
+        //   ],
+        // ),
       ),
     );
   }
-}
+} 
